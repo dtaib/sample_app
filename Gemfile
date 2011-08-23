e4bf88ee8434c969fccb029a80aa4c6c2d8728ff
Dzulkiflee Taib <dzulkiflee.taib@gmail.com>
2011-08-23 23:55:31 +1000

    Added a user factory and test for the show page

--- Gemfile
+++ Gemfile
@@ -19,4 +19,5 @@ group :test do
 	 gem 'autotest-growl'
 	 gem 'ZenTest'
      gem 'spork'
+ 	 gem 'factory_girl_rails'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.name                  "Dzulkiflee Taib"
+  user.email                 "Dzulkiflee.Taib@gmail.com"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
